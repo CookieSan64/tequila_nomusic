090dbc12f32b34d3264e22555839f0a711472431
ChouCookieSan <choucookiesan@gmail.com>
2024-03-08 02:33:14 +0100

    Initial commit

---
+++ fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'cerulean'
+
+author 'Hassaric#8416'
+description 'Tequila-la'
+version '1.0.0'
+
+games { 'gta5' }
+
+client_scripts {
+    'client/tequilla.lua',
+}
+
+this_is_a_map 'yes'
